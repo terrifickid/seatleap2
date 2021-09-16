4f4e8660eb66e566883f01ae80d9086cd20e2b7e
terrifickid <tk@terrifickid.net>
2021-09-16 17:16:21 -0500

    update

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import './app.scss';
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+
+
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 text-center">
+          <h1 class="text-uppercase text-white mb-5">Upgrade your<br>seat for free<br>and chill</h1>
+          <p class="mb-2 blue">Enter your date of birth to continue:</p>
+        </div>
+      </div><!-- end col -->
+      <div class="col-10 offset-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+        <div class="row">
+          <div class="col"><input type="text" placeholder="MM" class="form-control text-center"></div>
+          <div class="col"><input type="text" placeholder="DD"  class="form-control text-center"></div>
+          <div class="col"><input type="text" placeholder="YYYY" class="form-control text-center"></div>
+        </div>
+      </div><!-- end col -->
+      <div class="col-10 offset-1 col-md-6 offset-md-3 col-lg-6 offset-lg-3 text-center">
+        <p class="mt-3 dblue" style="margin-bottom: 15rem;" ><small>Must be 21 years or older to participate. Upgrades are<br> available on a first come, first served basis.</small></p>
+      </div>
+      <div class="col-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+        <button class="btn bold btn-light btn-lg text-uppercase btn-block mb-4">Get My Seats</button>
+        <img src="../assets/coors/landing-bottom-image.png" class="img-fluid">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.home{
+  padding: 4rem 0rem 1rem 0rem;
+  background: url('../assets/coors/landing-bg.png');
+  background-size: cover;
+  background-position: center bottom;
+  min-height: 100vh;
+}
+
+h1{font-size: 2.5rem; font-weight: bold; font-family: 'brandon-grotesque'}
+</style>
